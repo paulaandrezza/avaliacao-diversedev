9c32b6fd58fdd11801df44054559ccffe5b2f120
paulaandrezza <paulaandrezza25@gmail.com>
2023-10-10 19:52:57 -0300

    feat: finish desafio02

--- desafios/Desafio01.cs
+++ desafios/Desafio01.cs
@@ -2,13 +2,44 @@
 using System.IO;
 using System.Runtime.Intrinsics.X86;
 
+// Considerei o 0 como positivo
+
 class Desafio01 : Desafio
 {
     public override void Execute()
     {
+        int[] numbers = new int[5];
+        int pares = 0,
+            positivos = 0;
+
+        bool validChoice = false;
         try
         {
-            Console.WriteLine("Hello World");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (!validChoice)
+                {
+                    Console.Write($"\nDigite o {i + 1}° numero: ");
+                    if (int.TryParse(Console.ReadLine(), out numbers[i]))
+                    {
+                        if (numbers[i] % 2 == 0)
+                            pares += 1;
+                        if (numbers[i] >= 0)
+                            positivos += 1;
+                        validChoice = true;
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("Opção inválida. Insira um número inteiro válido.");
+                    }
+                }
+                validChoice = false;
+            }
+
+            Console.WriteLine($"\nValores pares: {pares}");
+            Console.WriteLine($"Valores impares: {5 - pares}");
+            Console.WriteLine($"Valores positivos: {positivos}");
+            Console.WriteLine($"Valores negativos: {5 - positivos}");
         }
         catch (Exception ex)
         {
--- desafios/Desafio02.cs
+++ desafios/Desafio02.cs
@@ -6,9 +6,75 @@ class Desafio02 : Desafio
 {
     public override void Execute()
     {
+        int n1 = 0,
+            n2 = 0,
+            multiplicacao = 0,
+            divisao = 0;
+        bool validChoice = false,
+            negativo = false;
         try
         {
-            Console.WriteLine("Hello World");
+            while (!validChoice)
+            {
+                Console.Write("\nDigite o 1° numero: ");
+                if (int.TryParse(Console.ReadLine(), out n1))
+                {
+                    validChoice = true;
+                }
+                else
+                {
+                    Console.Error.WriteLine("Opção inválida. Insira um número inteiro válido.");
+                }
+            }
+            validChoice = false;
+
+            while (!validChoice)
+            {
+                Console.Write("\nDigite o 2° numero: ");
+                if (int.TryParse(Console.ReadLine(), out n2))
+                {
+                    if (n2 == 0)
+                    {
+                        Console.Error.WriteLine("Erro: Impossível dividir por zero.");
+                    }
+                    else
+                    {
+                        validChoice = true;
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine("Opção inválida. Insira um número inteiro válido.");
+                }
+            }
+
+            if (n1 < 0 && n2 > 0 || n1 > 0 && n2 < 0)
+            {
+                negativo = true;
+            }
+
+            n1 = Math.Abs(n1);
+            n2 = Math.Abs(n2);
+
+            for (int i = 0; i < n2; i++)
+            {
+                multiplicacao += n1;
+            }
+
+            while (n1 >= n2)
+            {
+                n1 -= n2;
+                divisao++;
+            }
+
+            if (negativo)
+            {
+                multiplicacao = multiplicacao * -1;
+                divisao = divisao * -1;
+            }
+
+            Console.WriteLine($"\nValor da multiplicacao: {multiplicacao}");
+            Console.WriteLine($"Valor da divisao: {divisao}");
         }
         catch (Exception ex)
         {
