56b92a91879b563eb6be544fc6b035b108984573
paulaandrezza <paulaandrezza25@gmail.com>
2023-10-10 19:18:23 -0300

    feat: init assessment

--- desafios/Desafio03.cs
+++ desafios/Desafio03.cs
@@ -2,20 +2,6 @@
 using System.IO;
 using System.Runtime.Intrinsics.X86;
 
-// Desafio 1: Calculadora
-
-// Objetivo: Escrever um programa que solicite ao usuário dois números e realize operações
-// simples de adição, subtração, multiplicação e divisão.
-
-// Instruções:
-
-// Solicite ao usuário que insira dois números.
-// Realize as operações de adição, subtração, multiplicação e divisão entre esses números.
-// Exiba os resultados das operações no console.
-
-// Dica: Use variáveis para armazenar os números de entrada e os resultados intermediários.
-
-
 class Desafio03 : Desafio
 {
     public override void Execute()
